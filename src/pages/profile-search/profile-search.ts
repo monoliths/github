8bd1812195487f4ca1b949cd752cf1c5e15cf294
Gerald S. Hernandez <HernandezGS@c-iv.org>
2017-05-25 15:50:43 -0700

    initial commit

---
+++ src/pages/profile-search/profile-search.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ProfileSearchPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-profile-search',
+  templateUrl: 'profile-search.html',
+})
+export class ProfileSearchPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ProfileSearchPage');
+  }
+
+}
