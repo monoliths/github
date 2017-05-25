a13f923a9a650f1c172ef612f8e907f71b73c8a4
Gerald S. Hernandez <HernandezGS@c-iv.org>
2017-05-25 16:27:47 -0700

    added search field and button

--- src/pages/profile-search/profile-search.ts
+++ src/pages/profile-search/profile-search.ts
@@ -14,6 +14,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfileSearchPage {
 
+  username: string = "";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
